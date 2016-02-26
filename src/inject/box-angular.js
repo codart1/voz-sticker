2ad64c5771eb8b0263e54b6cd7b41b8df535e3f9
ManhDT <manhdt@trams.co.kr>
2016-02-26 17:41:19 +0700

    tabs

--- src/inject/box-angular.js
+++ src/inject/box-angular.js
@@ -1,6 +1,5 @@
 function initBox(angular, element, other) {
 
-
 	var GIFPHY_API = {
 		host: 'https://api.giphy.com/',
 		apiKey: 'dc6zaTOxFJmzC',
@@ -31,6 +30,14 @@ function initBox(angular, element, other) {
 		}
 	];
 
+	var TabController = [
+		'$scope', 'StatesManager',
+		function($scope, stateManager) {
+			$scope.sm = stateManager;
+		}
+
+	];
+
 	//SERVICE
 	var GifphyService = [
 		'GIFPHY_API',
@@ -58,13 +65,116 @@ function initBox(angular, element, other) {
 		}
 	];
 
+	var TabsManager = [
+		function() {
+
+
+			return {
+
+			};
+		}
+	]
+
+	var StatesManager = [
+
+		function() {
+			var currentState = null;
+			var states = {
+				search: {
+					active: false,
+					text: 'Tìm kiếm',
+					faIconName: 'search'
+				},
+				favo: {
+					active: false,
+					text: 'Ưa thích',
+					faIconName: 'heart'
+				},
+				hot: {
+					active: false,
+					text: 'Hot',
+					faIconName: 'fire'
+				},
+				collection: {
+					active: false,
+					text: 'Bộ sưu tập',
+					faIconName: 'suitcase'
+				},
+				setting: {
+					active: false,
+					text: 'Cài đặt',
+					faIconName: 'cogs'
+				}
+			};
+
+			var orderedStates = [
+				states.search,
+				states.favo,
+				states.hot,
+				states.collection,
+				states.setting
+			];
+
+			function active(state) {
+				if(currentState) {
+					currentState.active = false;
+				}
+				state.active = true;
+				currentState = state;
+			}
+
+			function getCurrentState() {
+				return currentState;
+			}
+
+			function getAllStates() {
+				return states;
+			}
+
+			function getOrderedState() {
+				return orderedStates;
+			}
+
+			active(states.search);
+
+			return {
+				active: active,
+				getCurrentState: getCurrentState,
+				getAllStates: getAllStates,
+				getOrderedState: getOrderedState
+			};
+		}
+	]
+
+	var Run = [
+		'$rootScope',
+		function($rootScope) {
+			$rootScope.getUrl = function(url) {
+				return chrome.extension.getURL(url);
+			};
+		}
+	];
+
 	//DEFINE COMPONENT:
 	var app = angular.module('demo', [])
 		.controller('MainController', MainController)
+		.controller('TabController', TabController)
 
 		.factory('GifphyService', GifphyService)
+		.factory('TabsManager', TabsManager)
+		.factory('StatesManager', StatesManager)
 
-		.constant('GIFPHY_API', GIFPHY_API);
+		.constant('GIFPHY_API', GIFPHY_API)
+
+		.run(Run)
+
+		.filter('trusted', ['$sce', function ($sce) {
+		    return function(url) {
+		        return $sce.trustAsResourceUrl(url);
+		    };
+		}]);
+
+		;
 
 	angular.bootstrap(element, ['demo']);
 }
